e1c195d1926855be1bcf6235d82fceb916f40a68
Ami Domadia <ami@Amis-MacBook-Pro.local>
2019-04-23 20:08:35 -0400

    Login thunk

--- client/login.js
+++ client/login.js
@@ -1,35 +1,59 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
+import {loginThunk} from './store';
 
-const Login = () => {
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    
+  }
 
-  return (
-    <div className='h100 w100 flex column align-items-center justify-center'>
-      <h1>Let's Loggin'!</h1>
-      <div className='flex w50'>
-        <img src='/loggin.png' />
-        <form className='grow1'>
-          <div className='flex column'>
-            <div className='flex column m1'>
-              <label htmlFor='email'>Email</label>
-              <input type='email' name='email' className='input' />
+  onHandleChange= (event) => {
+    event.preventDefault();
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  onHandleSubmit=(event) => {
+    event.preventDefault();
+    this.props.login(this.state)
+      .catch((error)=>console.log(error))
+    
+  }
+  
+  render(){
+    return (
+      <div className='h100 w100 flex column align-items-center justify-center'>
+        <h1>Let's Loggin'!</h1>
+        <div className='flex w50'>
+          <img src='/loggin.png' />
+          <form className='grow1'>
+            <div className='flex column'>
+              <div className='flex column m1'>
+                <label htmlFor='email'>Email</label>
+                <input onChange={this.onHandleChange} type='email' name='email' className='input' value={this.state.email} />
+              </div>
+              <div className='flex column m1'>
+                <label htmlFor='email'>Password</label>
+                <input onChange={this.onHandleChange} type='password' name='password' className='input' value={this.state.password} />
+              </div>
+              <div className='m1'>
+                <button type='submit' onClick={this.onHandleSubmit} className='btn bg-blue white p1 rounded'>Submit</button>
+              </div>
             </div>
-            <div className='flex column m1'>
-              <label htmlFor='email'>Password</label>
-              <input type='password' name='password' className='input' />
-            </div>
-            <div className='m1'>
-              <button type='submit' className='btn bg-blue white p1 rounded'>Submit</button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    login: (user) => dispatch(loginThunk(user))
   };
 }
 
--- client/store.js
+++ client/store.js
@@ -1,14 +1,31 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux'
 import loggerMiddleware from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
+import axios from 'axios';
 
+const SET_USER = 'SET_USER';
 
-const userReducer = (state = { }, action) => {
+function setUser(user){
+  return {
+    type: SET_USER,
+    user
+  }
+}
+const userReducer = (state = {user: {} }, action) => {
+  switch(action.type){
+    case SET_USER: return {...state, user: action.user}
+  }
   return state;
 }
 
 const reducer = combineReducers({
   user: userReducer
 });
+
+export const loginThunk = (user) => dispatch => {
+  return axios.post('/auth/', user)
+              .then((res)=>res.data)
+              .then((user)=>dispatch(setUser(user)));
+}
 
 export default createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware))
